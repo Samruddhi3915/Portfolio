eb3467597f497cb59b202911deabbc1139ff5633
Samruddhi3915 <samruddhi3915@gmail.com>
2023-04-29 23:44:36 +0530

    Creating About us page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,57 @@
 import React from "react";
-
+import photo from "../images/sam.jpg";
+import { TypeAnimation } from "react-type-animation";
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container ">
+      <div className="row">
+        <div className="col col-lg-8 col-sm-12">
+          <div>
+            <h3>Hello!</h3>
+            <TypeAnimation
+              sequence={["I'm Samruddhi Ahire", 1000]}
+              wrapper="span"
+              cursor={false}
+              repeat={Infinity}
+              style={{
+                color: "red",
+                fontSize: "2.5em",
+                display: "inline-block",
+              }}
+            />
+            {/* <h2 className="p-5">Hello! This is Samruddhi Ahire.</h2> */}
+            <p style={{ color: "black", fontSize: "2em" }}>
+              I am a full-stack web developer with a keen interest in Problem
+              solving.
+            </p>
+            <center>
+              <span>
+                <i className="m-3 fa-solid fa-envelope fa-2xl"></i>
+                <i className=" m-3 fa-brands fa-square-github fa-2xl"></i>
+                <i className=" m-3 fa-brands fa-linkedin fa-2xl"></i>
+              </span>
+            </center>
+          </div>
+        </div>
+        <div className="col col-lg-4 col-sm-12">
+          <center>
+            <img
+              style={{ maxWidth: "400px", maxHeight: "400px" }}
+              width="360px"
+              height="360px"
+              className=" mt-4 ml-3 mb-3 rounded-circle border border-3 border-dark shadow "
+              src={photo}
+              alt=""
+            />
+            <div className="col-sm-12">
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </center>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,21 +1,51 @@
-import React, { useState } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
-import samplePDF from "./doc/resume.pdf";
+import React from "react";
+import img6 from "../images/resume.png";
+import pdfFile from "./doc/resume.pdf";
 export const Resume = () => {
-  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-  // const [numPages, setNumPages] = useState(null);
-  // const [pageNumber, setPageNumber] = useState(1);
-
-  // function onDocumentLoadSuccess({ numPages }) {
-  //   setNumPages(numPages);
-  //   setPageNumber(1);
-  // }
-  // onLoadSuccess = { onDocumentLoadSuccess };
   return (
-    <div className="container">
-      <Document file={samplePDF}>
-        <Page pageNumber={1} />
-      </Document>
+    <div className="conatiner ">
+      <center>
+        <img
+          className="p-2 m-4 border border-dark shadow"
+          src={img6}
+          alt="hey"
+        />
+      </center>
+      <center>
+        <button className="btn btn-success">
+          <a
+            href={pdfFile}
+            download
+            className="text-white text-decoration-none"
+          >
+            Download
+          </a>
+        </button>
+      </center>
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
+
+// import React, { useState } from "react";
+// import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+
+// const Resume = () => {
+//   const [NumPages, setnumPages] = useState(null);
+//   const [PageNumber, setPageNumber] = useState(1);
+//   function onDocumentSuccess({ NumPages }) {
+//     setnumPages(NumPages);
+//   }
+//   return (
+//     <div className="d-flex align-items-center justify-content-center">
+//       <Document file="./doc/resume.pdf" onLoadSuccess={onDocumentSuccess}>
+//         <Page pageNumber={1} width={1000} />
+//       </Document>
+//     </div>
+//   );
+// };
+
+// export default Resume;
