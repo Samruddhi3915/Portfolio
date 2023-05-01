84d2859dde159685f1bc96e6f19e7b27ab89c124
Samruddhi3915 <samruddhi3915@gmail.com>
2023-05-01 23:35:12 +0530

    Implementing Skills Tab

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,117 @@
 import React from "react";
-
+import cs from "../images/css-3.png";
 export const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <>
+      <div className="container">
+        <center>
+          <h1
+            className="mt-2"
+            style={{
+              fontFamily: "Lucida Console, Courier New, monospace",
+              fontWeight: 700,
+              color: "#36454F",
+            }}
+          >
+            Projects
+          </h1>
+          <hr />
+        </center>
+        <center>
+          <div className="row shadow p-4 m-3">
+            <div className="col-md-6 col-sm-12 mt-2 ml-2 p-2 shadow-sm">
+              <div class="card" style={{ width: "18rem" }}>
+                <img src={cs} class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="/" class="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-sm-12 mt-2 ml-2 p-2 shadow-sm">
+              <div class="card" style={{ width: "18rem" }}>
+                <img src={cs} class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="/" class="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-sm-12 mt-2 ml-2 p-2 shadow-sm">
+              <div class="card" style={{ width: "18rem" }}>
+                <img src={cs} class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="/" class="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-sm-12 mt-2 ml-2 p-2 shadow-sm">
+              <div class="card" style={{ width: "18rem" }}>
+                <img src={cs} class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="/" class="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-sm-12 mt-2 ml-2 p-2 shadow-sm">
+              <div class="card" style={{ width: "18rem" }}>
+                <img src={cs} class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="/" class="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-sm-12 mt-2 ml-2 p-2 shadow-sm">
+              <div class="card" style={{ width: "18rem" }}>
+                <img src={cs} class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="/" class="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </center>
+      </div>
+    </>
+  );
 };
