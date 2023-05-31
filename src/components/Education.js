e95e0ec3c76980057e7ac4fab29856fc73e3d2f7
Samruddhi3915 <samruddhi3915@gmail.com>
2023-05-31 22:17:29 +0530

    Updations

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,5 +1,149 @@
 import React from "react";
-
+import fc from "../images/francis (2).jpg";
+import kp from "../images/kkwp.jpeg";
+import pt from "../images/pict.jpg";
 export const Education = () => {
-  return <div>Education</div>;
+  return (
+    <>
+      <div classNameName="container">
+        <center>
+          <h1
+            className="mt-2"
+            style={{
+              fontFamily: "Lucida Console, Courier New, monospace",
+              fontWeight: 700,
+              color: "#36454F",
+            }}
+          >
+            Education
+          </h1>
+          <hr />
+        </center>
+        <center>
+          <div className="row shadow p-5 m-3  border-2">
+            <div className="col-md-4 col-sm-12 mt-2 ml-2 p-2 shadow-sm ">
+              <div
+                className="card rounded shadow p-2"
+                style={{ width: "24rem" }}
+              >
+                <h5
+                  className="card-title p-2 m-2"
+                  style={{
+                    fontFamily: "Lucida Console, Courier New, monospace",
+                    fontWeight: 900,
+                    fontSize: "30px",
+                  }}
+                >
+                  Schooling
+                </h5>
+                <center>
+                  <img
+                    src={fc}
+                    className="img-fluid shadow"
+                    alt="..."
+                    border="2px"
+                    width="280px"
+                    height="200px"
+                  />
+                </center>
+                <br />
+                <h4
+                  style={{
+                    fontFamily: "Lucida Console, Courier New, monospace",
+                    fontWeight: 900,
+                    fontSize: "20px",
+                  }}
+                >
+                  St. Francis High School,Nashik
+                </h4>
+                <div className="card-body">
+                  <p className="card-text">SSC Percentage: 97.02%</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-12 mt-2 ml-2 p-2 shadow-sm ">
+              <div
+                className="card rounded shadow p-2"
+                style={{ width: "24rem" }}
+              >
+                <h5
+                  className="card-title p-2 m-2"
+                  style={{
+                    fontFamily: "Lucida Console, Courier New, monospace",
+                    fontWeight: 900,
+                    fontSize: "30px",
+                  }}
+                >
+                  Schooling
+                </h5>
+                <center>
+                  <img
+                    src={kp}
+                    className="img-fluid"
+                    alt="..."
+                    border="2px"
+                    width="280px"
+                    height="200px"
+                  />
+                </center>
+                <br />
+                <h4
+                  style={{
+                    fontFamily: "Lucida Console, Courier New, monospace",
+                    fontWeight: 900,
+                    fontSize: "19px",
+                  }}
+                >
+                  K.K.Wagh Polytechnic,Nashik
+                </h4>
+                <div className="card-body">
+                  <p className="card-text">Diploma Percentage: 97.71%</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-12 mt-2 ml-2 p-2 shadow-sm ">
+              <div
+                className="card rounded shadow p-2"
+                style={{ width: "24rem" }}
+              >
+                <h5
+                  className="card-title p-2 m-2"
+                  style={{
+                    fontFamily: "Lucida Console, Courier New, monospace",
+                    fontWeight: 900,
+                    fontSize: "30px",
+                  }}
+                >
+                  Engineering
+                </h5>
+                <center>
+                  <img
+                    src={pt}
+                    className="img-fluid"
+                    alt="..."
+                    border="2px"
+                    width="280px"
+                    height="200px"
+                  />
+                </center>
+                <br />
+                <h4
+                  style={{
+                    fontFamily: "Lucida Console, Courier New, monospace",
+                    fontWeight: 900,
+                    fontSize: "20px",
+                  }}
+                >
+                  PICT,Pune
+                </h4>
+                <div className="card-body">
+                  <p className="card-text">CGPA: 9.43</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </center>
+      </div>
+    </>
+  );
 };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,10 @@
 import React from "react";
-import cs from "../images/css-3.png";
+import inn from "../images/Inotebook.png";
+import ic from "../images/Icode.png";
+import tc from "../images/Tindog.png";
+import tsh from "../images/tshirt.png";
+import tu from "../images/textutils.png";
+import nm from "../images/newsmonkey.png";
 export const Projects = () => {
   return (
     <>
@@ -18,99 +23,219 @@ export const Projects = () => {
           <hr />
         </center>
         <center>
-          <div className="row shadow p-4 m-3">
-            <div className="col-md-6 col-sm-12 mt-2 ml-2 p-2 shadow-sm">
-              <div class="card" style={{ width: "18rem" }}>
-                <img src={cs} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+          <div className="row shadow p-4 m-3 border border-dark border-2">
+            <div className="col-md-6 col-sm-12 mt-2 ml-2 p-2 shadow-sm ">
+              <div className="card shadow" style={{ width: "28rem" }}>
+                <h5
+                  className="card-title p-2 m-2"
+                  style={{
+                    fontFamily: "Lucida Console, Courier New, monospace",
+                    fontWeight: 900,
+                    fontSize: "30px",
+                  }}
+                >
+                  ICode
+                </h5>
+                <img src={ic} className="card-img-top p-2 shadow" alt="..." />
+                <div className="card-body">
+                  <p className="card-text" style={{ textAlign: "left" }}>
+                    Icode is a unified coding platform built using MERN Stack
+                    where users can showcase their coding profiles on
+                    leetcode,codechef and codeforces.
                   </p>
-                  <a href="/" class="btn btn-primary">
-                    Go somewhere
+                  <a
+                    href="https://icode-c3mw.onrender.com/"
+                    className="btn btn-primary"
+                  >
+                    View Project
                   </a>
                 </div>
               </div>
             </div>
             <div className="col-md-6 col-sm-12 mt-2 ml-2 p-2 shadow-sm">
-              <div class="card" style={{ width: "18rem" }}>
-                <img src={cs} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+              <div className="card shadow" style={{ width: "28rem" }}>
+                <h5
+                  className="card-title p-2 m-2"
+                  style={{
+                    fontFamily: "Lucida Console, Courier New, monospace",
+                    fontWeight: 900,
+                    fontSize: "30px",
+                  }}
+                >
+                  T-Store
+                </h5>
+                <img
+                  src={tsh}
+                  className="card-img-top p-2 shadow"
+                  alt="..."
+                  width="130px"
+                  height="360px"
+                />
+                <div className="card-body">
+                  <p className="card-text" style={{ textAlign: "left" }}>
+                    T-Store is an online T-shirt built using MERN Stack which is
+                    a selling website wherein we have T-shirts according to
+                    seasons. It is built using MERN Stack and has a payment
+                    gateway implemented (Braintree Paypal).
                   </p>
-                  <a href="/" class="btn btn-primary">
-                    Go somewhere
+                  <a
+                    href="https://github.com/Samruddhi3915/T-Store"
+                    className="btn btn-primary"
+                  >
+                    View Project
                   </a>
                 </div>
               </div>
             </div>
             <div className="col-md-6 col-sm-12 mt-2 ml-2 p-2 shadow-sm">
-              <div class="card" style={{ width: "18rem" }}>
-                <img src={cs} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+              <div className="card shadow" style={{ width: "28rem" }}>
+                <h5
+                  className="card-title p-2 m-2"
+                  style={{
+                    fontFamily: "Lucida Console, Courier New, monospace",
+                    fontWeight: 900,
+                    fontSize: "30px",
+                  }}
+                >
+                  NewsMonkey
+                </h5>
+                <img
+                  src={nm}
+                  className="card-img-top p-2 shadow"
+                  width="130px"
+                  height="230px"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <p className="card-text" style={{ textAlign: "left" }}>
+                    NewsMonkey is a React App that helps you stay upto date
+                    about current happenings in the world in various categories
+                    including:
+                    Business,Entertainment,General,Health,Science,Sports,
+                    Technology
                   </p>
-                  <a href="/" class="btn btn-primary">
-                    Go somewhere
+                  <a
+                    href="https://github.com/Samruddhi3915/NewsMonkey"
+                    className="btn btn-primary"
+                  >
+                    View Project
                   </a>
                 </div>
               </div>
             </div>
             <div className="col-md-6 col-sm-12 mt-2 ml-2 p-2 shadow-sm">
-              <div class="card" style={{ width: "18rem" }}>
-                <img src={cs} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+              <div className="card shadow" style={{ width: "28rem" }}>
+                <h5
+                  className="card-title p-2 m-2"
+                  style={{
+                    fontFamily: "Lucida Console, Courier New, monospace",
+                    fontWeight: 900,
+                    fontSize: "30px",
+                  }}
+                >
+                  TextUtils
+                </h5>
+                <img
+                  src={tu}
+                  className="card-img-top p-2 shadow"
+                  alt="..."
+                  width="130px"
+                  height="230px"
+                />
+                <div className="card-body">
+                  <p className="card-text" style={{ textAlign: "left" }}>
+                    <br />
+                    TextUtils is a React App that allows a user to manipulate
+                    the text according to their choice.It is built using
+                    ReactJS. One can convert text to Lower case and Upper
+                    case,copy text,clear text,remove extra spaces and users can
+                    see the preview of their text.
                   </p>
-                  <a href="/" class="btn btn-primary">
-                    Go somewhere
+                  <a
+                    href="https://github.com/Samruddhi3915/TextUtils"
+                    className="btn btn-primary"
+                  >
+                    View Project
                   </a>
                 </div>
               </div>
             </div>
             <div className="col-md-6 col-sm-12 mt-2 ml-2 p-2 shadow-sm">
-              <div class="card" style={{ width: "18rem" }}>
-                <img src={cs} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+              <div className="card shadow" style={{ width: "28rem" }}>
+                <div className="card-body">
+                  <h5
+                    className="card-title p-2 m-2"
+                    style={{
+                      fontFamily: "Lucida Console, Courier New, monospace",
+                      fontWeight: 900,
+                      fontSize: "30px",
+                    }}
+                  >
+                    Tindog
+                  </h5>
+                  <img
+                    src={tc}
+                    className="card-img-top p-2 shadow "
+                    alt="..."
+                    width="130px"
+                    height="230px"
+                  />
+                  <p className="card-text" style={{ textAlign: "left" }}>
+                    <br />
+                    Tindog is a simple web application built with HTML, CSS, and
+                    Bootstrap that allows users to browse and connect with other
+                    dog owners in their area.
                   </p>
-                  <a href="/" class="btn btn-primary">
-                    Go somewhere
+                  <a
+                    href="https://github.com/Samruddhi3915/Tindog"
+                    className="btn btn-primary"
+                  >
+                    View Project
                   </a>
                 </div>
               </div>
             </div>
             <div className="col-md-6 col-sm-12 mt-2 ml-2 p-2 shadow-sm">
-              <div class="card" style={{ width: "18rem" }}>
-                <img src={cs} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+              <div className="card shadow" style={{ width: "28rem" }}>
+                <div className="card-body">
+                  <h5
+                    className="card-title p-2 m-2"
+                    style={{
+                      fontFamily: "Lucida Console, Courier New, monospace",
+                      fontWeight: 900,
+                      fontSize: "30px",
+                    }}
+                  >
+                    INotebook
+                  </h5>
+                  <img
+                    src={inn}
+                    className="card-img-top p-2 shadow"
+                    alt="..."
+                    width="130px"
+                    height="220px"
+                  />
+                  <p className="card-text" style={{ textAlign: "left" }}>
+                    <br />
+                    INotebook is a application through which one can keep their
+                    notes on cloud. It is built using MERN Stack.The user can
+                    perform the basic CRUD operations on his/her notes using the
+                    App.
                   </p>
-                  <a href="/" class="btn btn-primary">
-                    Go somewhere
+                  <a
+                    href="https://github.com/Samruddhi3915/inotebook"
+                    className="btn btn-primary"
+                  >
+                    View Project
                   </a>
                 </div>
               </div>
             </div>
           </div>
         </center>
+        <br />
+        <br />
+        <br />
       </div>
     </>
   );
